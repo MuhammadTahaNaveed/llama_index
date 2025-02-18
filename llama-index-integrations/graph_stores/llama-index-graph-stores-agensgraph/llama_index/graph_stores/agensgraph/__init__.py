1c3bca18d89c02f9b200854c148dfec64526ee0d
Muhammad Taha Naveed <m.taha.naveed27@gmail.com>
2025-02-18 12:58:26 +0500

    Cleanup and add tests for AgensGraphStore

---
+++ llama-index-integrations/graph_stores/llama-index-graph-stores-agensgraph/llama_index/graph_stores/agensgraph/__init__.py
@@ -0,0 +1,7 @@
+from llama_index.graph_stores.agensgraph.base import AgensGraphStore
+# from llama_index.graph_stores.kuzu.kuzu_property_graph import (
+#     KuzuPropertyGraphStore,
+# )
+
+# __all__ = ["KuzuGraphStore", "KuzuPropertyGraphStore"]
+__all__ = ["AgensGraphStore"]
